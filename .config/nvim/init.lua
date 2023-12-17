65e95471d52b7c8980d29b70045e159cbc44ed7a
Masahiro Shibata <mshibatatt@gmail.com>
2023-12-18 01:10:21 +0900

    setup mason and cmp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -49,11 +49,24 @@ require('lazy').setup({
     },
     {
         'hrsh7th/nvim-cmp',
-        dependencies = { 'hrsh7th/cmp-nvim-lsp' }
+        dependencies = { 
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/cmp-vsnip',
+            'hrsh7th/vim-vsnip',
+        }
     },
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.5',
         dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        'williamboman/mason.nvim'
+    },
+    {
+        'williamboman/mason-lspconfig.nvim'
     }
 })
 
@@ -100,15 +113,42 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 -- Set up autocompletion
-local cmp = require('cmp')
+local cmp = require'cmp'
 cmp.setup({
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        end,
+    },
     mapping = cmp.mapping.preset.insert({
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm { select = false },
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' },
+    }, {
+        { name = 'buffer' },
+    })
+})
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
 })
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -120,3 +160,14 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- Set up LSP manager
+require('mason').setup()
+require('mason-lspconfig').setup()
+require('mason-lspconfig').setup_handlers {
+    function(server_name)
+        require('lspconfig')[server_name].setup {
+            capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        }
+    end,
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# dotfiles
+Neovim base setting
+
+usage example
+```
+# download dotfiles
+git clone --depth 1 https://github.com/mshibatatt/dotfiles.git
+rm -rf dotfiles/.git
+
+# move docker-compose
+# include Dockerfile into Dockerfile.nvim
+mv dotfiles/docker-compose.yml docker-compose.yml
+mv dotfiles/Dockerfile.nvim Dockerfile.nvim
+
+# run
+docker compose up --build
+docker compose run editor bash
+```
+
+example in case use nvim-remote
+```
+# run nvim in server
+docker compose run -p 8888:8888 editor nvim --headless --listen 0.0.0.0:8888
+# connect from another terminal
+nvim --remote-ui --server localhost:8888
+```
+
