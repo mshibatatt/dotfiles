de676bca3dfdb8254b1ac39373cf611899b260b0
Masahiro Shibata <38758355+mshibatatt@users.noreply.github.com>
2024-01-20 15:07:36 +0900

    Update install_nvim.sh

--- .scripts/install_nvim.sh
+++ .scripts/install_nvim.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+cd ..
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage 
 chmod u+x nvim.appimage 
 ./nvim.appimage --appimage-extract 
