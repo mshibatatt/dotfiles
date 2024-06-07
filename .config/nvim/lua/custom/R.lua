fcd635aedd25ac7c5bf84bb74eae2de91e308bb9
Masahiro Shibata <mshibatatt@gmail.com>
2024-06-08 02:21:51 +0900

    add custom in lua file

--- .config/nvim/lua/custom/R.lua
+++ .config/nvim/lua/custom/R.lua
@@ -1,7 +1,11 @@
 -- R.lua 
 -- setup for R
 require("cmp_r").setup({ })
-
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'r-languageserver',
+  }
+})
 -- indent size is 2 in R
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
