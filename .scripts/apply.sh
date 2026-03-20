92037435d7b72c8d012b706db1ddd3a66b75fda1
Masahiro Shibata <mshibatatt@gmail.com>
2026-03-20 17:38:54 +0900

    change parent option

--- .scripts/apply.sh
+++ .scripts/apply.sh
@@ -4,25 +4,25 @@ script_dir=$(cd "$(dirname "$0")" && pwd)
 cd $script_dir
 cd ..
 
-cp --parents .config/nvim/init.lua ~/
-cp --parents .config/nvim/lua/plugins/default.lua ~/
+cp -P .config/nvim/init.lua ~/
+cp -P .config/nvim/lua/plugins/default.lua ~/
 cp .tmux.conf ~/
 
 # copy custom folder depending on option
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     -r|--R)
-      cp --parents .config/nvim/lua/plugins/R.lua ~/
-      cp --parents .config/nvim/lua/custom/R.lua ~/
+      cp -P .config/nvim/lua/plugins/R.lua ~/
+      cp -P .config/nvim/lua/custom/R.lua ~/
       echo "require('custom.R')" >> ~/.config/nvim/init.lua
       ;;
     -py|--Python)
-      cp --parents .config/nvim/lua/plugins/Python.lua ~/
-      cp --parents .config/nvim/lua/custom/Python.lua ~/
+      cp -P .config/nvim/lua/plugins/Python.lua ~/
+      cp -P .config/nvim/lua/custom/Python.lua ~/
       echo "require('custom.Python')" >> ~/.config/nvim/init.lua
       ;;
     -rs|--Rust)
-      cp --parents .config/nvim/lua/custom/Rust.lua ~/
+      cp -P .config/nvim/lua/custom/Rust.lua ~/
       echo "require('custom.Rust')" >> ~/.config/nvim/init.lua
       ;;
     *)
